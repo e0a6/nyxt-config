efed017b8e8b2d6915a450fc8ab1062db1297083
e0a6 <git@cheema.engineer>
2021-10-15 05:30:13 -0600

    Added new colour changes
    Added colour to Message Buffer (Echo Area) and Prompt
    Buffer (Minibuffer)

--- init.lisp
+++ init.lisp
@@ -11,6 +11,48 @@
 (define-configuration buffer
   ((default-modes (append '(nyxt::emacs-mode) %slot-default%))))
 
+;;; Message buffer is the small line down below where all the messages
+;;; are displayed. echo-area in Emacs parlance?
+(define-configuration window
+  ((message-buffer-style
+    (str:concat
+     %slot-default%
+     (cl-css:css
+      '((body
+         :background-color "black"
+         :color "white")))))))
+
+;;; Color config for prompt-buffer (minibuffer in Emacs parlance).
+(define-configuration prompt-buffer
+  ((style (str:concat
+           %slot-default%
+           (cl-css:css
+            '((body
+               :background-color "black"
+               :color "white")
+              ("#prompt-area"
+               :background-color "black")
+              ;; The area you input text in.
+              ("#input"
+               :background-color "white")
+              (".source-name"
+               :color "black"
+               :background-color "#556B2F")
+              (".source-content"
+               :background-color "black")
+              (".source-content th"
+               :border "1px solid #556B2F"
+               :background-color "black")
+              ;; The currently highlighted option.
+              ("#selection"
+               :background-color "#CD5C5C"
+               :color "black")
+              (.marked :background-color "#8B3A3A"
+                       :font-weight "bold"
+                       :color "white")
+              (.selected :background-color "black"
+                         :color "white")))))))
+
 ;;; Status buffer is the strip above the message buffer/echo area.
 ;;; Modeline in Emacs parlance.
 (define-configuration status-buffer
